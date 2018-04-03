e15053cd40c9f5bef2d63bab0c0578889c08201b
João Rocha <joao.delmas.rocha@pt.ey.com>
2018-04-03 14:33:05 +0100

    Working on Find GitHub User

--- src/components/cardItem.jsx
+++ src/components/cardItem.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Card, Icon, Image, Label } from 'semantic-ui-react';
+import mockRepo from './mocks/mockRepo';
 
 class CardItem extends Component {
 
@@ -12,7 +13,7 @@ class CardItem extends Component {
                 <Image src={image} size='medium' circular />
                 <Card.Content>
                     {/* <Label attached="top left">#{mockRepo.items[0].index}</Label> */}
-                    <Label attached="top left">#{index + 30 * (activePage - 1)}</Label>
+                    { index && <Label attached="top left">#{index + 30 * (activePage - 1)}</Label>}
                     <Card.Header>{login}</Card.Header>
                     <Card.Description className="card-description" href={html_url}>{name}</Card.Description>
                 </Card.Content>
--- src/components/find-user.jsx
+++ src/components/find-user.jsx
@@ -1,81 +1,75 @@
 import React, { Component } from 'react';
-import { Input, Button } from 'semantic-ui-react';
+import { Input, Button, Grid, Header, Icon } from 'semantic-ui-react';
 import CardItem from './cardItem';
 import { getUser } from '../services/apiService';
 
 class FindUser extends Component {
+  state = {
+    inputText: '',
+    image: '',
+    login: '',
+    name: '',
+    watchers: '',
+    html_url: ''
+  };
 
-    state = {
-        inputText: '',
-        image: '',
-        login: '',
-        name: '',
-        watchers: '',
-        html_url: ''
-    };
+  handleChange = event => {
+    this.setState({ inputText: event.target.value });
+  };
 
-    handleChange = event => {
-        console.log(event.target.value);
-        this.setState({ inputText: event.target.value });
-       
-    };
+  handleSubmit = async event => {
+    const user = await this.searchUser(this.state.inputText);
 
-    handleSubmit = (event) => { 
-        console.log("Handling Submit..!");
-        event.persist();
-        console.log('InputText is => ', this.state.inputText);
-        const user = this.searchUser(this.state.inputText);
-        this.setState({
-            image: user.avatar_url,
-            login: user.login,
-            name: user.name,
-            watchers: user.followers,
-            html_url: user.html_url
-        })
-    };
+    this.setState({
+      image: user.avatar_url,
+      login: user.login,
+      name: user.name,
+      watchers: user.followers,
+      html_url: user.html_url
+    });
+  };
 
-    async searchUser(username) {
-        try {
-            const response = await getUser(username);
-            const data = response.data;
-            console.log("Got this user => ", data);
-            return data;
-        } catch (error) {
-            console.log(error);
-        }
+  async searchUser(username) {
+
+    try {
+
+      const response = await getUser(username);
+      const data = response.data;
+      return data;
+
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    render() {
+  render() {
     const { image, login, name, watchers, html_url } = this.state;
-    // console.log("state is => ", this.state);
 
-        return (
-            <div>
-                <h2> Find GitHub User</h2>
-                <Input 
-                    placeholder="Search..." 
-                    onChange={this.handleChange}
-                    >
-                </Input>
-                <Button onClick={this.handleSubmit}>Search...</Button>
-                <CardItem 
-                    index={1}
-                    activePage={1}
-                    /* login={login}
-                    image={image}
-                    name={name}
-                    watchers={watchers}
-                    html_url={html_url} */
-                    login="joaodarocha"
-                    image="https://avatars3.githubusercontent.com/u/27374026?v=4"
-                    name="João da Rocha"
-                    watchers={2}
-                    html_url="https://github.com/joaodarocha"
-
-                ></CardItem>
-            </div>
-        );
-    }
+    return (
+      <div>
+        <Header as="h1" icon>
+            <Icon name="github" />
+            Find GitHub User
+        </Header>
+        <Grid columns="1" centered>
+          <Grid.Row>
+            <Input placeholder="Search..." onChange={this.handleChange} />
+            <Button onClick={this.handleSubmit}>Search...</Button>
+          </Grid.Row>
+          { this.state.login &&
+          <Grid.Row>
+            <CardItem
+              login={login}
+              image={image}
+              name={name}
+              watchers={watchers}
+              html_url={html_url}
+            />
+          </Grid.Row>}
+        </Grid>
+      </div>
+    );
+  }
 }
 
 export default FindUser;
